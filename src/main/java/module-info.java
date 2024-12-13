94093e53a4d08d27b7a497372bd792ce51f7cda6
Ibrahim_Kibria <1611048@iub.edu.bd>
2024-12-13 12:42:24 +0600

    inital commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module csc213.cse213_finalproject_group21_bksp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens csc213.cse213_finalproject_group21_bksp to javafx.fxml;
+    exports csc213.cse213_finalproject_group21_bksp;
+}
